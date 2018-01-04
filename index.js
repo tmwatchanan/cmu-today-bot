daa79f4ab7957d343db8424368cc537f21f50ae6
tmwatchanan <tmwatchanan@gmail.com>
2018-01-04 20:47:19 +0700

    Add webhook

--- index.js
+++ index.js
@@ -1,81 +1,62 @@
-// Reference the packages we require so that we can use them in creating the bot
-var restify = require('restify');
-var builder = require('botbuilder');
-// =========================================================
-// Bot Setup
-// =========================================================
+'use strict';
 
-// Setup Restify Server
-// Listen for any activity on port 3978 of our local server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
-});
-// Create chat bot
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
-var bot = new builder.UniversalBot(connector);
+// Imports dependencies and set up http server
+const
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express().use(bodyParser.json()); // creates express http server
 
-// If a Post request is made to /api/messages on port 3978 of our local server, then we pass it to the bot connector to handle
-server.post('/api/messages', connector.listen());
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
-//=========================================================
-// Bots Middleware
-//=========================================================
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {
+    let body = req.body;
 
-// Anytime the major version is incremented any existing conversations will be restarted.
-bot.set(builder.Middleware.dialogVersion({ version: 1.0, resetCommand: /^reset/i }));
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
 
-//=========================================================
-// Bots Global Actions
-//=========================================================
+        // Iterates over each entry - there may be multiple if batched
+        body.entry.forEach(function (entry) {
 
-bot.endConversationAction('goodbye', 'Goodbye :)', { matches: /^goodbye/i });
-bot.beginDialogAction('help', '/help', { matches: /^help/i });
+            // Gets the message. entry.messaging is an array, but 
+            // will only ever contain one message, so we get index 0
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+        });
 
-// =========================================================
-// Bots Dialogs 
-// =========================================================
-// This is called the root dialog. It is the first point of entry for any message the bot receives
-bot.dialog('/', function (session) {
-
-    // Send 'hello world' to the user
-    session.send("Hello World");
-
+        // Returns a '200 OK' response to all requests
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        // Returns a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+    }
 });
 
-// Add dialog fpr flipping a coin
-bot.dialog('flipCoinDialog', [
-    function (session, args) {
-        builder.Prompts.choice(session, "Choose heads or tails.", "heads|tails", { listStyle: builder.ListStyle.none })
-    },
-    function (session, results) {
-        var flip = Math.random() > 0.5 ? 'heads' : 'tails';
-        if (flip == results.response.entity) {
-            session.endDialog("It's %s. YOU WIN!", flip);
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = "theverifytokenofcmutodaybotisthis"
+
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
         } else {
-            session.endDialog("Sorry... It was %s. you lost :(", flip);
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
         }
     }
-]).triggerAction({ matches: /flip/i });
-
-// Add dialog for rolling some dice
-bot.dialog('rollDiceDialog', [
-    function (session, args) {
-        builder.Prompts.number(session, "How many dice should I roll?");
-    },
-    function (session, results) {
-        if (results.response > 0) {
-            var msg = "I rolled:";
-            for (var i = 0; i < results.response; i++) {
-                var roll = Math.floor(Math.random() * 6) + 1;
-                msg += ' ' + roll.toString();
-            }
-            session.endDialog(msg);
-        } else {
-            session.endDialog("Ummm... Ok... I rolled air.");
-        }
-    }
-]).triggerAction({ matches: /roll/i });
+});
