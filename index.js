a6e80bdf229c69c1296d57e206f6115ddf7b80a3
tmwatchanan <tmwatchanan@gmail.com>
2018-01-04 21:04:41 +0700

    Add urlencoded option  of body parser for express

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const
     express = require('express'),
     bodyParser = require('body-parser'),
-    app = express().use(bodyParser.json()); // creates express http server
+    app = express().use(bodyParser.json()).use(bodyParser.urlencoded()); // creates express http server
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
